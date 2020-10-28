4881eecc095c1388536e87cd2cb697f2da8c4cdf
ahrohn92 <ahrohn92@gmail.com>
2020-10-28 19:51:50 +0100

    refactored/reorganized code

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 const {argv} = require('yargs')
 
 class Game {
-
     constructor(playerMove, computerMove, winningMoves) {
         this.playerMove = playerMove
         this.computerMove = computerMove
         this.winningMoves = winningMoves
         this.result = ''
     }
-
     play() {
         this.playerMove === this.computerMove ? this.result = '~The game was a tie.~' : 
         (this.computerMove === this.winningMoves[this.playerMove] ? this.result = '~Computer wins.~' : this.result = '~Player wins.~')
@@ -16,12 +14,10 @@ class Game {
 }
 
 class Output {
-    
     constructor(game) {
         this.game = game
         game.play()
     }
-
     displayOutput() {
         console.log('Playing a game of Roshambo against the computer')
         console.log('Player plays '+game.playerMove+'!')
@@ -30,16 +26,9 @@ class Output {
     }
 }
 
-const playerMove = argv.move
 const moves = ['rock', 'paper', 'scissors']
-const computerMove = moves[Math.floor(Math.random() * moves.length)]
 
-var winningMoves = {
-    'rock': 'paper',
-    'paper': 'scissors',
-    'scissors': 'rock'
-}
-
-var game = new Game(playerMove, computerMove, winningMoves)
+var game = new Game(argv.move, moves[Math.floor(Math.random() * moves.length)], 
+winningMoves = {'rock':'paper','paper':'scissors','scissors':'rock'})
 var output = new Output(game)
 output.displayOutput()
