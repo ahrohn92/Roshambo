038f4a633bf1dd5cf17dd85726cfc9cf89ebd527
ahrohn92 <ahrohn92@gmail.com>
2020-10-28 20:06:23 +0100

    final commit

--- main.js
+++ main.js
@@ -16,19 +16,25 @@ class Game {
 class Output {
     constructor(game) {
         this.game = game
-        game.play()
+        this.game.play()
     }
     displayOutput() {
         console.log('Playing a game of Roshambo against the computer')
-        console.log('Player plays '+game.playerMove+'!')
-        console.log('Computer plays '+game.computerMove+'!')
-        console.log(game.result)
+        console.log('Player plays '+this.game.playerMove+'!')
+        console.log('Computer plays '+this.game.computerMove+'!')
+        console.log(this.game.result)
     }
 }
 
+const playerMove = argv.move
 const moves = ['rock', 'paper', 'scissors']
+const computerMove = moves[Math.floor(Math.random() * moves.length)]
+const winningMoves = {
+    'rock': 'paper',
+    'paper': 'scissors',
+    'scissors': 'rock'
+}
 
-var game = new Game(argv.move, moves[Math.floor(Math.random() * moves.length)], 
-winningMoves = {'rock':'paper','paper':'scissors','scissors':'rock'})
+var game = new Game(playerMove, computerMove, winningMoves)
 var output = new Output(game)
 output.displayOutput()
